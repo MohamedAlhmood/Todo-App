27d5c7e5a82543493199631ff6e08b06fec85e66
Mohamed Alhmood <Moealhmood21@gmail.com>
2024-09-02 23:54:47 -0400

    Created Add feature

--- gui.py
+++ gui.py
@@ -1,8 +1,21 @@
 import functions
-import FreeSimpleGUI as fs
-label = fs.Text("Type in a todo")
-input_box=fs.InputText(tooltip="enter a todo")
-button = fs.Button("add")
-window =fs.Window("My Todo App",layout=[[[label],input_box,button]])
-window.read()
+import FreeSimpleGUI as sg
+
+
+label = sg.Text("Type in a todo")
+input_box=sg.InputText(tooltip="enter a todo",key='todo')
+button = sg.Button("add")
+window =sg.Window("My Todo App",layout=[[[label]
+                                            ,input_box,button]]
+                                            ,font=('Helvetica',20))
+while True:
+    event,values= window.read()
+    match event:
+        case 'add':
+            todos=functions.get_todos()
+            new_todo=values['todo']+"\n"
+            todos.append(new_todo)
+            functions.writeTodos(todos)
+        case sg.WIN_CLOSED:
+            break
 window.close()
